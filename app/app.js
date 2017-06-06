33c04671f826fb32f2bfea2c46818143ba48e7fb
Andrew Rock <Andrew Rock>
2017-06-06 10:37:38 -0500

    basic route structure to app.js

--- app/app.js
+++ app/app.js
@@ -3,15 +3,21 @@
 var app = angular.module("ImageApp", ['ngRoute']);
 
 app.config(function($routeProvider){
-	$routeProvider.
-	when('/', {
-		templateUrl: '',
+	$routeProvider
+	.when('/', {
+		templateUrl: 'partials/home-view.html',
 		controller: ''
-	}).
-	when('/', {
-		templateUrl: '',
+	})
+	.when('/profile', {
+		templateUrl: 'partials/profile-view.html',
 		controller: ''
-	}).
-	otherwise('/');
+	})
+	.when('/pindetails', {
+		templateUrl: 'partials/large-view.html'
+	})
+	.when('/explore', {
+		templateUrl: 'partials/explore-view.html'
+	})
+	.otherwise('/');
 });
 
