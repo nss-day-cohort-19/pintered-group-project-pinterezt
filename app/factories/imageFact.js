136ca4dd11086f024ba59372829c88e1ce2e6c67
Azim <azimsadikov@gmail.com>
2017-06-08 12:33:56 -0500

    flickr search works boys

--- app/factories/imageFact.js
+++ app/factories/imageFact.js
@@ -17,8 +17,9 @@ app.factory("ImageFactory", function($q, $http, FBCreds, AuthFactory) {
         });
 	};
 	const getSearchedImages = (query) => {
+		console.log('flickr query', query);
         return $q((resolve, reject) => {
-                $.getJSON(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=2a3c22dabde799119ef12641ea2ef046&text=${query}&per_page=50&format=json&nojsoncallback=1&auth_token=72157682813718420-28c021ce6691cb48&api_sig=7a7c54dde6ce52a6e782fffb5916b9d9`, {
+                $.getJSON(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=9b64377a28fcf6b100a77d639060a84f&tags=${query}&per_page=50&format=json&nojsoncallback=1`, {
                         format: "json"
                 })
             .then((itemObj) => {
